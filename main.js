630267cc8e1d9d3c7024a8cf10a6ff1f09914076
ranranqi <1137625794@qq.com>
2018-10-09 18:46:41 +0800

    实现自动轮播，高亮，鼠标进入图片停住离开轮播

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-$(p1).on('click', function () {
-    $(images).css({
-        transform: 'translateX(0)'
+var allButtons = $("#buttons>button")
+for (let i = 0; i < allButtons.length; i++) {
+    $(allButtons[i]).on('click', function (x) {
+        var index = $(x.currentTarget).index()
+        var p = index * -260
+        $('#images').css({
+            transform: 'translateX(' + p + 'px)'
+        })
+        n = index
+        allButtons.eq(n)
+            .addClass('red')
+            .siblings('.red')
+            .removeClass('red')
     })
+}
+
+var n = 0
+var size = allButtons.length
+allButtons.eq(n % size).trigger('click').addClass('red')
+    .siblings('.red').removeClass('red')
+
+var timerId = setInterval(() => {
+    n += 1
+    allButtons.eq(n % size).trigger('click').addClass('red')
+        .siblings('.red').removeClass('red')
+}, 1000)
+
+
+$('.window').on('mouseenter', function () {
+    window.clearInterval(timerId)
 })
-$(p2).on('click', function () {
-    $(images).css({
-        transform: 'translateX(-260px)'
-    })
-})
-$(p3).on('click', function () {
-    $(images).css({
-        transform: 'translateX(-520px)'
-    })
-})
-$(p4).on('click', function () {
-    $(images).css({
-        transform: 'translateX(-780px)'
-    })
+$('.window').on('mouseleave', function () {
+    timerId = setInterval(() => {
+        n += 1
+        allButtons.eq(n % size).trigger('click').addClass('red')
+            .siblings('.red').removeClass('red')
+    }, 1000)
 })
